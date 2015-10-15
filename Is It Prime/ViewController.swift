345cab0dbaeaeaccb9e9b445258fc302f6f6c2b7
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-10-14 23:31:51 -0400

    Completed app

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var input_TextField: UITextField!
+    @IBOutlet var answer_Label: UILabel!
+    
+    @IBAction func submit_Button(sender: AnyObject) {
+        
+        var isPrime = true
+        let input = Int (input_TextField.text!)!
+        
+        if input > 2 {
+            for var i = 2; i < input; i++ {
+                if input % i == 0 {
+                    isPrime = false
+                }
+                
+            }
+        } else if input == 2 {
+            isPrime = true
+        } else {
+            isPrime = false
+        }
+        
+        print(isPrime)
+        
+        if isPrime {
+            answer_Label.text = "Yes!"
+        } else {
+            answer_Label.text = "Nope!"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
